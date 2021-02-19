76a54e14d92473bc6428dbbb40bbd9ef0abd1a9a
prernakakria123 <kakria99@gmail.com>
2021-02-19 22:56:17 +0530

    Updated

--- main.js
+++ main.js
@@ -14,9 +14,7 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
-  });
 }
 
 app.on('ready', () => {
